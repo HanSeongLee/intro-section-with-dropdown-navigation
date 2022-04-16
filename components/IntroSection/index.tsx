f71216bc63ec1b4d5ebd92f06496f8befcec28ca
HanSeong Lee <jkhanseong@gmail.com>
2022-04-16 16:56:07 +0900

    ADD | IntroSection on desktop

--- components/IntroSection/index.tsx
+++ components/IntroSection/index.tsx
@@ -8,18 +8,21 @@ const IntroSection: React.FC = () => {
     return (
         <section className={styles.introSection}>
             <picture>
-                <source src={'/img/image-hero-mobile.png'}
-                        media={'(min-width: 568px)'}
+                <source srcSet={'/img/image-hero-mobile.png'}
+                        media={'(max-width: 568px)'}
+                />
+                <source srcSet={'/img/image-hero-desktop.png'}
+                        media={'(max-width: 767px)'}
                 />
                 <img className={styles.heroImage}
-                     src={'/img/image-hero-mobile.png'}
+                     src={'/img/image-hero-desktop.png'}
                      alt={''}
                 />
             </picture>
 
-            <Container>
+            <Container className={styles.container}>
                 <h2 className={styles.title}>
-                    Make remote work
+                    Make <br/>remote work
                 </h2>
                 <p className={styles.description}>
                     Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.
