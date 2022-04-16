f5dc95bac53b4e84c15da9e31fd019851ac7a346
HanSeong Lee <jkhanseong@gmail.com>
2022-04-16 17:33:32 +0900

    ADD | className to Backdrop

--- components/Backdrop/index.tsx
+++ components/Backdrop/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {HTMLAttributes} from 'react';
 import styles from './style.module.scss';
+import cn from 'classnames';
 
-const Backdrop: React.FC = ({ children, ...props }) => {
+const Backdrop: React.FC<HTMLAttributes<HTMLDivElement>> = ({ className, children, ...props }) => {
     return (
-        <div className={styles.backdrop}
+        <div className={cn(styles.backdrop, className)}
              {...props}
         >
             {children}
