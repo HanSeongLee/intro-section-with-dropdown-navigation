2d43e313a5684f97cebac913c7dcf80a578017bf
HanSeong Lee <jkhanseong@gmail.com>
2022-04-16 16:10:00 +0900

    ADD | Main page on mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from 'next'
+import styles from './style.module.scss';
 import Head from 'next/head';
+import Header from "../components/Header";
+import IntroSection from "../components/IntroSection";
 
 const Home: NextPage = () => {
   return (
@@ -53,7 +56,9 @@ const Home: NextPage = () => {
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png"/>
         </Head>
 
-        <main>
+        <main className={styles.main}>
+          <Header />
+          <IntroSection />
         </main>
       </>
   );
